0b7a61498df822cb2a6d5cc2dcc1d1de0cd7ace1
iQuixotic <treyw1850@gmail.com>
2019-02-12 21:29:06 -0600

    names appear

--- php/getData.php
+++ php/getData.php
@@ -8,13 +8,27 @@ $query = "SELECT * FROM employees";
 // WHERE id = '" . $_POST["id"] . "'";
 $result = mysqli_query($mysqli, $query);
 
+// class Sql {
+//     public $name;
+//     public $id;
+//     public function __construct() {
+//         $this->id = $id;
+//         $this->name = $name;
+//     }
+    
+// }
+$rows = array();
+
 while($row = mysqli_fetch_assoc($result)) {
-    $arr = array(
-        $data['id'] => $row['id'];
-        $data['name'] = $row['name'];
-    );
-    echo json_encode($arr);
+    // $myEmp = new stdClass();
+    // $myEmp->id = $row['id'];
+    // $myEmp->name = $row['name'];
+    $rows[] = $row['name'];
+
+    
+    // echo "id: {$row['id']} name: {$row['name']} ";
 }    
+echo json_encode($rows);
 
 
 
@@ -26,3 +40,11 @@ while($row = mysqli_fetch_assoc($result)) {
     //     $data['phone']  = $row['phone'];
     // }
 // }
+
+
+
+// while($row = mysqli_fetch_array($result)) {
+//     $rows[] = "id: {$row['id']} name: {$row['name']} ";
+//     // echo $rows;
+//     echo json_encode($rows);
+// }    
--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,18 @@ $(document).ready(() => {
 $.ajax({
     url: 'http://localhost/employee_cards/php/getData.php',
     method: "GET",
+    dataType: "json",
     success: function (data) {
         console.log(data)
-        // let res = JSON.stringify(data.toString())
-        // console.log('res', res)
-        // console.log('data', JSON.stringify(data.toString()))
-            // for (let i = 0; i < data.length; i++) {
-                // console.log('arrData', arrData[i])
-            //     let thisOption = [];
-            //     let emp = [];
-            //     thisOption[i] = `<a id='EMP-dropdown-option[${i}]' class="dropdown-item" href="#"></a>`;
-            //     console.log('thisoptionati', thisOption[i])
-            //     $(`#EMP-dropdown-option[${i}]`).html(emp[i]) 
-            //     $('.dropdown-menu').append(thisOption[i])
-            // }
+
+            for (let i = 0; i < data.length; i++) {
+                let thisOption = [];
+                let emp = [];
+                thisOption[i] = `<a id='EMP-dropdown-option[${i}]' class="dropdown-item" href="#">${data[i]}</a>`;
+                console.log('datI', data[i].name)
+                $(`#EMP-dropdown-option[${i}]`).html(emp[i]) 
+                $('.dropdown-menu').append(thisOption[i])
+            }
         }
     });
     
