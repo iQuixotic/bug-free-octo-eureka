d15928357f77b51c33c8d8bf34fc5f4df59c5792
iQuixotic <treyw1850@gmail.com>
2019-02-13 01:15:30 -0600

    $id query works

--- php/getData.php
+++ php/getData.php
@@ -3,7 +3,8 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/employee_cards/php/Employee.php';
     
     /* $mysql is ready */
-$query = "SELECT * FROM employees WHERE id = $data->id";
+$id = $_GET['id'];
+$query = "SELECT * FROM employees WHERE id = $id";
 $result = mysqli_query($mysqli, $query);
 
 $rows = array();
@@ -11,11 +12,10 @@ $rows = array();
 while($row = mysqli_fetch_assoc($result)) {
     $rows[] = array(
     'id' => $row['id'], 
-    'name' => $row['name'], 
-    'position' => $row['position'],
+    'name' => $row['name'],
+    'position' => $row['position'], 
     'phone' => $row['phone'],
-    'email' => $row['email']
-    );
+    'email' => $row['email']);    
 }    
 
 echo json_encode($rows);
--- public/js/main.js
+++ public/js/main.js
@@ -19,22 +19,31 @@ $.ajax({
     }); 
 
     $(document).on('click', 'a', (e) => {
-        let target = e.target.id.slice(6);
-        console.log(target);
+        let target = e.target.id.slice(6)
         
         $.ajax({
             url: 'http://localhost/employee_cards/php/getData.php',
             method: "GET",
             dataType: "json",
-            // cache:true,
+            cache:true,
                 data: {
                   id: target
                 },            
             success: function (data) {
-                console.log(data)
+                console.log('this made it all the way around !!! ', data)
+                console.log(data[0]);
+                // console.log(JSON.parse(data.id));
+                console.log(data.id);
+
+
+                console.log(`<h2>${data[0].name}</h2>`);
+                console.log(`<h2>${data[0].id}</h2>`);
+                console.log(`<h2>${data[0].position}</h2>`);
+                console.log(`<h2>${data[0].phone}</h2>`);
+                console.log(`<h2>${data[0].email}</h2>`);
+
             }
-        }); 
-        
+        });         
     });
     
 });
